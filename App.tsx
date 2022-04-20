9af9e40a255d3fa1d3d23bc9dbcb62f366eba7a6
ariclavel <a01706629@itesm.mx>
2022-04-20 19:42:30 -0400

    tarea

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Principal from "./Components/Principal";
+import Detalle from "./Components/Detalle";
 
-export default function App() {
+
+
+function App() {
+  const Stack = createNativeStackNavigator();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className="App">
+      <NavigationContainer linking={{enabled: true}}>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen 
+          name="Principal"
+          component={Principal}
+        />
+        <Stack.Screen 
+          name="Detalle"
+          component={Detalle}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </div>
   );
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+
