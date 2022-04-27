08aaf34d530d59d290c76eb509594a82ea5a078e
ariclavel <a01706629@itesm.mx>
2022-04-27 13:38:30 -0400

    nueva tarea

--- Flask/app.py
+++ Flask/app.py
@@ -2,8 +2,14 @@
 from flask import Flask, jsonify
 from markupsafe import escape
 from flask_db2 import DB2
-import sys
-from flask_cors import CORS
+import sys 
+from flask_cors import CORS, cross_origin
+import sqlalchemy 
+from sqlalchemy import * 
+import ibm_db_sa
+import flask_login
+import secrets 
+import flask
 
 #creamos un objeto de tipo flask
 app= Flask (__name__)
@@ -19,6 +25,67 @@ app.config['DB2_PASSWORD'] = 'hola'
 db = DB2(app)
 
 CORS(app)
+
+# CÓDIGO PARA LOGIN 
+# necesitamos una llave secreta 
+app.secret_key = secrets.token_urlsafe(16)
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+# PSEUDO BASE DE DATOS DE USUARIOS
+usuarios = {"a@a.com" : {"pass" : "hola"}}
+
+# definir una clase para contener la descripción de nuestros usuarios
+class Usuario(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    # verificar vs fuente de datos 
+    if email not in usuarios:
+        return
+    
+    usuario = Usuario()
+    usuario.id = email
+    return usuario
+# método que se invoca para obtención de usuarios cuando se hace request
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in usuarios:
+        return
+    
+    usuario = Usuario()
+    usuario.id = email
+    return usuario
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # podemos verificar con qué método se accedió 
+    if flask.request.method == 'GET':
+        return '''
+                <form action='login' method='POST'>
+                    <input type='text' name='email' /><br />
+                    <input type='password' name='password' /><br />
+                    <input type='submit' name='HACER LOGIN' />
+                </form>
+        
+        '''
+    # de otra manera tuvo que ser POST
+    # obtener datos 
+    email = flask.request.form['email']
+    # verificar validez de usuario vs fuente de datos
+    if email in usuarios and flask.request.form['password'] == usuarios[email]['pass']:
+        user = Usuario()
+        user.id = email
+        flask_login.login_user(user)
+        # OJO AQUI
+        # esta es la parte en donde pueden generar un token
+        # return flask.redirect(flask.url_for('protegido'))
+        return "USUARIO VALIDO"
+
+    # si no jaló mostrar error
+    return "CREDENCIALES INVÁLIDAS"
+    
 
 #le agregamos rutas
 #@ en python significa que vamos a usar un decorator
